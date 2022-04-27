31921c3deb539f6cddf1a57f56741e8273899bca
Stepianek <marcin.lasecki99@gmail.com>
2022-04-27 19:48:02 +0200

    remove unneccessary promise in github service

--- src/services/github.service.ts
+++ src/services/github.service.ts
@@ -17,107 +17,85 @@ export interface ReposAndUsersMixin {
 }
 
 class GithubService {
-  private static readonly baseUrl = "https://api.github.com";
-
   static searchRepositoriesByPhrase(
     phrase: string
   ): Promise<GithubRespositoriesResponse> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const { data } = await instance.get<GithubRespositoriesResponse>(
-          `${this.baseUrl}/search/repositories?q=${encodeURIComponent(
-            phrase
-          )}&per_page=10`
-        );
-        resolve(data);
-      } catch (err) {
+    return instance
+      .get<GithubRespositoriesResponse>(
+        `/search/repositories?q=${encodeURIComponent(phrase)}&per_page=10`
+      )
+      .then(({ data }) => data)
+      .catch((err) => {
         if (axios.isAxiosError(err)) {
-          reject(new HttpException(Number(err.response?.status), err.message));
+          throw new HttpException(Number(err.response?.status), err.message);
         }
-        reject(err);
-      }
-    });
+        throw err;
+      });
   }
 
   static searchUsersByPhrase(phrase: string): Promise<GithubUsersResponse> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const { data } = await instance.get<GithubUsersResponse>(
-          `${this.baseUrl}/search/users?q=${encodeURIComponent(
-            phrase
-          )}&per_page=10`
-        );
-        resolve(data);
-      } catch (err) {
+    return instance
+      .get<GithubUsersResponse>(
+        `/search/users?q=${encodeURIComponent(phrase)}&per_page=10`
+      )
+      .then(({ data }) => data)
+      .catch((err) => {
         if (axios.isAxiosError(err)) {
-          reject(
-            new HttpException(
-              Number(err.response?.status),
-              err.response?.data.message
-            )
+          throw new HttpException(
+            Number(err.response?.status),
+            err.response?.data.message
           );
         }
-        reject(err);
-      }
-    });
+        throw err;
+      });
   }
 
   static getUserByLogin(login: string): Promise<UserByLogin> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const { data } = await instance.get<UserByLogin>(
-          `${this.baseUrl}/users/${encodeURIComponent(login)}`
-        );
-        resolve(data);
-      } catch (err) {
+    return instance
+      .get<UserByLogin>(`/users/${encodeURIComponent(login)}`)
+      .then(({ data }) => data)
+      .catch((err) => {
         if (axios.isAxiosError(err)) {
-          reject(
-            new HttpException(
-              Number(err.response?.status),
-              err.response?.data.message
-            )
+          throw new HttpException(
+            Number(err.response?.status),
+            err.response?.data.message
           );
         }
-        reject(err);
-      }
-    });
+        throw err;
+      });
   }
 
-  static searchUsersAndReposByPhrase(
+  static async searchUsersAndReposByPhrase(
     phrase: string
   ): Promise<ReposAndUsersMixin> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const res = await Promise.all([
-          this.searchRepositoriesByPhrase(phrase),
-          this.searchUsersByPhrase(phrase),
-        ]);
+    try {
+      const res = await Promise.all([
+        this.searchRepositoriesByPhrase(phrase),
+        this.searchUsersByPhrase(phrase),
+      ]);
 
-        //We need to make additional request for each user to get required fields (full name, location)
-        const users = await Promise.all(
-          res[1].items.map((user) => this.getUserByLogin(user.login))
+      //We need to make additional request for each user to get required fields (full name, location)
+      const users = await Promise.all(
+        res[1].items.map((user) => this.getUserByLogin(user.login))
+      );
+
+      const items = [...res[0].items, ...users].sort((a, b) => a.id - b.id);
+
+      return {
+        incomplete_results:
+          res[0].incomplete_results || res[1].incomplete_results,
+        total_count: res[0].total_count + res[1].total_count,
+        items,
+      };
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        throw new HttpException(
+          Number(err.response?.data.status),
+          err.response?.data.message
         );
-
-        const items = [...res[0].items, ...users].sort((a, b) => a.id - b.id);
-
-        resolve({
-          incomplete_results:
-            res[0].incomplete_results || res[1].incomplete_results,
-          total_count: res[0].total_count + res[1].total_count,
-          items,
-        });
-      } catch (err) {
-        if (axios.isAxiosError(err)) {
-          reject(
-            new HttpException(
-              Number(err.response?.data.status),
-              err.response?.data.message
-            )
-          );
-        }
-        reject(err);
       }
-    });
+      throw err;
+    }
   }
 }
 
